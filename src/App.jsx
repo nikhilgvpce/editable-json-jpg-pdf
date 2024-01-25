51cd92cc3e65cfc9dbe2fd8bb465e71a08060c48
nikhilgvpce <nikhil.gvpce@gmail.com>
2024-01-25 18:11:42 +0530

    implement leftpanel and display extracted data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import ReviewScreen from './ReviewScreen/ReviewScreen';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+
+    async function getPages(url) {
+      const pagesRawData = await fetch(url);
+      const pagesJson = await pagesRawData.json();
+      console.log('pagesJson', pagesJson)
+      const pageData = extractPagesInfo(pagesJson)
+      setPages(pageData);
+    }
+    getPages('./data/pages.json');
+  }, [])
+
+  
+
+  function extractPagesInfo(pages) {
+    const pageData = pages.data.documents.map((page) => {
+      return {
+        docId: page.doc_id,
+        title: page.title,
+        type: page.type,
+        status: page.status
+      }
+    })
+    return pageData;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ReviewScreen pages={pages} />
     </>
   )
 }
